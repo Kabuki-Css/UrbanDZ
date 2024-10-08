30cf257e7e6f29eed3f85aae531216c36cb48148
Kabuki-DevelopmentRU <57896520+Kabuki-Css@users.noreply.github.com>
2024-10-08 03:23:04 +0300

    module_2_4
    Выполнение домашнего задания модуль 2 задание 4

---
+++ module_2_4.py
@@ -0,0 +1,26 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# Создадим пустые списки
+primes = []
+not_primes = []
+
+# Создаём перебор
+for num in numbers:
+    is_prime = True
+    if num < 2:
+        is_prime = False
+    
+# Создаём проверку на простое число    
+    for divisor in range(2, int(num**0.5) + 1):
+        if num % divisor == 0:
+            is_prime = False
+            break 
+    
+    # Ракспределяем числа по спискам
+    if is_prime:
+         primes.append(num)
+    else:
+        not_primes.append(num)
+# Выводим списки
+print("Primes:", primes)
+print("Not Primes:", not_primes)
+
