c232eeb465b95a4a5e9054d79374b4c0692372ad
Kabuki-DevelopmentRU <57896520+Kabuki-Css@users.noreply.github.com>
2024-11-18 12:01:23 +0300

    Create module_6_1.py

---
+++ module_6_1.py
@@ -0,0 +1,71 @@
+# Родительский класс Animal
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.alive = True
+        self.fed = False
+
+    def eat(self, food):
+        if isinstance(food, Plant):
+            if food.edible:
+                print(f"{self.name} съел {food.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food.name}")
+                self.alive = False
+        else:
+            print(f"{self.name} не может есть {type(food).__name__}")
+
+# Родительский класс Plant
+class Plant:
+    def __init__(self, name):
+        self.name = name
+        self.edible = False
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+rabbit = Mammal("Кролик")
+tiger = Predator("Тигр")
+rose = Flower("Роза")
+apple = Fruit("Яблоко")
+
+# Новые объекты
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+print(a1.name)
+
+print(p1.name)
+
+
+
+print(a1.alive)
+
+print(a2.fed)
+
+a1.eat(p1)
+
+a2.eat(p2)
+
+print(a1.alive)
+
+print(a2.fed)
+
