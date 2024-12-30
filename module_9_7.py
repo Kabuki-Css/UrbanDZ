bc9c6797b4c7b75a66250ec4d438b2a3574c6a8e
Kabuki-DevelopmentRU <57896520+Kabuki-Css@users.noreply.github.com>
2024-12-30 13:17:00 +0300

    Create module_9_7.py

---
+++ module_9_7.py
@@ -0,0 +1,29 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    for i in range(3, int(number**0.5) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if is_prime(result):
+            print("Простое")
+        else:
+            print("Составное")
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+# Пример использования
+result = sum_three(2, 3, 6)
+print(result)
